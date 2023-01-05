4ea8e68874ea0de869b698391b4fe01619e8a1d2
Belcia <ikiza0010@gmail.com>
2023-01-05 09:49:26 +0100

    interfaces

---
+++ storage_types.hpp
@@ -0,0 +1,32 @@
+#ifndef STORAGE_TYPES
+#define STORAGE_TYPES
+
+#include <list>
+
+enum class PackageQueueType
+{
+    FIFO,
+    LIFO
+};
+
+class IPackageStockPile
+{
+public:
+    using const_iterator = std::list<Package>::const_iterator;
+    virtual void push() = 0;
+    virtual unsigned int size() const = 0;
+    virtual bool empty() const = 0;
+    virtual ~IPackageStockPile() = default;
+    virtual std::list<int>::const_iterator cbegin() const = 0;
+    virtual std::list<int>::const_iterator cend() const = 0;
+    virtual std::list<int>::const_iterator begin() const = 0;
+    virtual std::list<int>::const_iterator end() const = 0;
+};
+class IPackageQueue
+{
+public:
+    virtual Package pop() = 0;
+    virtual PackageQueueType get_queue_type() const = 0;
+};
+
+#endif
