85a228c2366b49c3f92e1d4e8486316fc11085c9
Belcia <ikiza0010@gmail.com>
2023-01-23 21:55:52 +0100

    możliwe że w tym momencie psuje nam projekt pozdrawiam

--- src/factory.cpp
+++ src/factory.cpp
@@ -9,12 +9,71 @@ enum class SenderStatus
 
 // FACTORY
 
-bool Factory::is_consistent() { return false; } // na wiki ZPO jest algorytm jak oni to chca
+bool has_reachable_storehouse(PackageSender *sender, std::map<PackageSender *, SenderStatus> &statuses)
+{
+    if (statuses[sender] == SenderStatus::Verified)
+        return true;
+
+    statuses[sender] = SenderStatus::Verified;
+
+    if (sender->receiver_preferences_.begin() == sender->receiver_preferences_.end())
+        throw std::logic_error("Graph is diconnected");
+
+    bool alo = false;
+    auto prefs = sender->receiver_preferences_;
+
+    for (auto r : prefs.get_preferences())
+    {
+        auto type = r.first->get_receiver_type();
+        if (type == ReceiverType::WORKER)
+        {
+            IPackageReceiver *r_ptr = r.first;
+            auto w_ptr = dynamic_cast<Worker *>(r_ptr);
+            auto s_ptr = dynamic_cast<PackageSender *>(w_ptr);
+            if (s_ptr == sender || s_ptr == nullptr)
+                continue;
+
+            alo = true;
+            if (statuses[s_ptr] == SenderStatus::NotVisited)
+                has_reachable_storehouse(s_ptr, statuses);
+        }
+    }
+
+    statuses[sender] = SenderStatus::Verified;
+    if (alo)
+        return true;
+    else
+        throw std::logic_error("Graph is diconnected");
+}
+
+bool Factory::is_consistent()
+{
+    std::map<PackageSender *, SenderStatus> statuses;
+
+    for (auto it = &(*worker_collection.begin()), end = &(*worker_collection.end()); it < end; it++)
+        statuses.insert({it, SenderStatus::NotVisited});
+
+    for (auto it = &(*ramp_collection.begin()), end = &(*ramp_collection.end()); it < end; it++)
+        statuses.insert({it, SenderStatus::NotVisited});
+
+    try
+    {
+        for (auto it = &(*ramp_collection.begin()), end = &(*ramp_collection.end()); it < end; it++)
+            if (!has_reachable_storehouse(dynamic_cast<PackageSender *>(it), statuses))
+                return false;
+    }
+    catch (const std::logic_error &)
+    {
+        return false;
+    }
+
+    return true;
+}
 
 void Factory::do_deliveries(Time t)
 {
     for (auto ramp = ramp_collection.begin(); ramp < ramp_collection.end(); ramp++)
-        ramp->deliver_goods(t)
+        ramp->deliver_goods(t);
 }
 
 void Factory::do_package_passing()
